16b9ecfb499877f68838e569007e05b885287dde
Carlos Moreno <omorenodovale@gmail.com>
2024-02-02 15:13:03 -0300

    feat: Insertion content type for all endpoints

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/carlos-moreno/api-go-rest/controllers"
+	"github.com/carlos-moreno/api-go-rest/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriaNovaPersonalidade).Methods("Post")
